5ccaba0ec594f96866103aab505e159be2258b21
jaianee <jaiane.furtado@outlook.com>
2022-02-27 12:22:15 -0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int maior = 0;
+        int soma = 0;
+
+        int count = 0;
+        do{
+            System.out.println("Numero: ");
+            numero = scan.nextInt();
+
+            soma = soma + numero;
+
+            if(numero > maior) maior = numero;
+
+            count++;
+        } while (count < 5);
+
+        System.out.println("Maior: " + maior);
+        System.out.println("Média: " + (soma/5));
+    }
+}
